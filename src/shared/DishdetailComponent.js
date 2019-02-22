8f9d49dbcb9fcc4fa7e008d57b507438007cd39a
Harsh Raj <ryder.harsh@.com>
2019-02-23 01:53:47 +0530

    dish page updated

---
+++ src/shared/DishdetailComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle, CardBody, CardText } from 'reactstrap';
+
+class dish extends Component {
+
+    renderDish(dish) {
+        if (dish != null) {
+            return(
+             
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+}
+export default dish;
